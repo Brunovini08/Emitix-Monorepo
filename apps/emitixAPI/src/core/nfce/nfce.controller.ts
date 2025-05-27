1ec1af60eeebb982eee1d170af82958d665f33bb
Brunovini08 <brunovinicius082005@gmail.com>
2025-05-27 16:02:09 -0300

    chore: Add more routes nfce, but yet missing some

--- apps/emitixAPI/src/core/nfce/nfce.controller.ts
+++ apps/emitixAPI/src/core/nfce/nfce.controller.ts
@@ -1,7 +1,224 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  Headers,
+  Request,
+  BadRequestException,
+  UseGuards,
+} from '@nestjs/common';
+import type { Base64 } from 'node-forge';
+import { Response } from 'express';
+import { AuthGuard } from 'src/core/auth/jwt-auth.guard';
+import { IssuerService } from 'src/core/issuer/issuer.service';
 import { NfceService } from './services/nfce.service';
+import type { NFeDto } from '../nfe/reusable/types/complex_types/TNFe/NFe.dto';
+import type { TEnviConsReciNFe } from '../nfe/reusable/types/complex_types/TCons/TEnviConsReciNFe';
+import type TEnvInutNfe from '../nfe/reusable/types/complex_types/TInut/TEnvInutNfe';
+import type TEnvConsSitNfe from '../nfe/reusable/types/complex_types/TCons/TEnvConsSitNfe';
+import type TEnvConsStatServ from '../nfe/reusable/types/complex_types/TCons/TEnvConsStatServ';
+import type { TEnvDistDFeInt } from '../nfe/reusable/types/complex_types/TDist/TEnvDistDFeInt';
+import type { TEnvEvento } from '../nfe/reusable/types/complex_types/TEvento/TEnvEvento';
 
-@Controller('nfce')
+@Controller('nfe')
 export class NfceController {
-  constructor(private readonly nfceService: NfceService) {}
+  constructor(private nfceService: NfceService, private issuerInvoiceService: IssuerService) { }
+
+  @UseGuards(AuthGuard)
+  @Post('emitir')
+  async create(
+    @Body() nfe: NFeDto,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+    @Headers('indSinc') indSinc: string,
+  ) {
+    const issuerInvoice = await this.issuerInvoiceService.create({
+      cnpj: String(nfe?.NFe?.infNFe?.emit?.CNPJ),
+      razaoSocial: String(nfe?.NFe?.infNFe?.emit?.xNome),
+      userId: req.user?.sub,
+    })
+    if (!issuerInvoice) {
+      throw new BadRequestException('Emitente não encontrado.');
+    }
+    if (!certificate || !password) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    const userId = req.user?.sub;
+    try {
+      const xml = await this.nfceService.create(
+        nfe,
+        certificate,
+        password,
+        userId,
+        indSinc,
+        5,
+        issuerInvoice,
+        "65"
+      );
+      res.send(xml)
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('consultaProcessamento')
+  async consultaProcessamento(
+    @Body() body: TEnviConsReciNFe,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ) {
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password || !certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const xml = await this.nfceService.consultaProcessamento(
+        body,
+        certificate,
+        password,
+        6,
+        "65"
+      );
+      res.send(xml);
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('inutilizar')
+  async inutilizar(
+    @Body() body: TEnvInutNfe,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ) {
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password || !certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const xml = await this.nfceService.inutilizarNFe(
+        body,
+        certificate,
+        password,
+        0,
+        "65"
+      )
+      res.send(xml);
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('consulta')
+  async consulta(
+    @Body() body: TEnvConsSitNfe,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ) {
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password || !certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const xml = await this.nfceService.consultaNFe(body, certificate, password, 1, "65")
+      res.send(xml)
+    } catch (error) {
+      console.error(error)
+      throw new BadRequestException(error)
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('statusservico')
+  async statusServico(
+    @Body() body: TEnvConsStatServ,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ) {
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password || !certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const xml = await this.nfceService.statusServico(body, certificate, password, 2, "65")
+      res.send(xml)
+    } catch (error) {
+      console.error(error)
+      throw new BadRequestException(error)
+    }
+  }
+
+
+
+  @UseGuards(AuthGuard)
+  @Post('distribuicaodfce')
+  async distribuicaoDfe(
+    @Body() body: TEnvDistDFeInt,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ) {
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password ||!certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const xml = await this.nfceService.distribuicaoDfe(body, certificate, password, 0, "65") 
+      res.send(xml)
+    } catch (error) {
+      console.error(error)
+      throw new BadRequestException(error)
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('evento')
+  async evento(
+    @Body() body: TEnvEvento,
+    @Res() res: Response,
+    @Request() req,
+    @Headers('password') password: string,
+    @Headers('certificate') certificate: Base64,
+  ){
+    if (!body) {
+      throw new BadRequestException('As informações são obrigatórias.');
+    }
+    if (!password ||!certificate) {
+      throw new BadRequestException('Certificado e senha são obrigatórios.');
+    }
+    try {
+      const userId = req.user?.sub;
+      const xml = await this.nfceService.evento(body, certificate, password, 4, userId, "65")
+      res.send(xml)
+    } catch (error) {
+      console.error(error)
+      throw new BadRequestException(error)
+    } 
+  }
+
 }
--- apps/emitixAPI/src/core/nfce/nfce.module.ts
+++ apps/emitixAPI/src/core/nfce/nfce.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NfceService } from './services/nfce.service';
 import { NfceController } from './nfce.controller';
+import { NotaService } from 'src/resources/middlewares/nota/nota.service';
+import { IdLoteService } from '../nfe/services/idLote.service';
+import { BuildService } from 'src/resources/middlewares/build/build.service';
+import { IssuerService } from '../issuer/issuer.service';
 
 @Module({
   controllers: [NfceController],
-  providers: [NfceService],
+  providers: [NfceService, NotaService, IdLoteService, BuildService, IssuerService],
 })
 export class NfceModule {}
