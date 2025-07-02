b2a786ab78ad6b41205b9ee06dbaf9b78e115fa6
Brunovini08 <brunovinicius082005@gmail.com>
2025-07-02 14:37:31 -0300

    feat(domain): Foi adicionado o Mapper da entidade de Consulta para fazer a adaptação dos dados recebidos

--- apps/emitixAPI/src/core/nfe/domain/interfaces/IIndicadorIEDest.ts
+++ apps/emitixAPI/src/core/nfe/domain/interfaces/IIndicadorIEDest.ts
@@ -1,9 +1,3 @@
 export interface IIndicadorIEDest {
-  /**
-   * Indicador da IE do destinatário
-   * 1 - Contribuinte ICMS
-   * 2 - Não Contribuinte
-   * 9 - Outros
-   */
   indIEDest: '0' | '1' | '2' | '9';
 }
